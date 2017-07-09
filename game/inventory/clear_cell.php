34d34f374a28a6b580252faeae104ca81579cd09
MikaMouseMan <n.mika.a@mail.ru>
2017-07-09 15:27:35 +0400

    now work correctly

--- game/inventory/clear_cell.php
+++ game/inventory/clear_cell.php
@@ -9,7 +9,7 @@
     $user_name = $_SESSION['user_name'];
     $form_user = "user_$user_name";
     
-    mysql_query("UPDATE `$form_user` SET `item_name` = 'empty', `item_count` = '0', `item_coef` = '0', `item_type` = '', `item_structure` = '' WHERE `$form_user`.`cell_id` = '$cell'");
+    mysql_query("UPDATE `$form_user` SET `item_name` = '', `item_count` = '0', `item_coef` = '0', `item_type` = '', `item_structure` = '', `health` = '0', `health_max` = '0' WHERE `$form_user`.`cell_id` = '$cell'");
     
     header('Location: inventory.php?msg=Item deleted!');
 
