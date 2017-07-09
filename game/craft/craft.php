4dbdd47b0eac9d1714946dad4779d70452aa1659
MikaMouseMan <n.mika.a@mail.ru>
2017-07-09 15:28:48 +0400

    add armore and tools link

--- game/craft/craft.php
+++ game/craft/craft.php
@@ -9,5 +9,7 @@
     <br><a href="craft_material/craft_material_select.php">Craft material</a>
     <br><a href="craft_component/craft_component_select.php">Craft component</a>
     <br><a href="craft_weapon/craft_weapon_select.php">Craft weapon</a>
+    <br><a href="craft_armore/craft_armore_select.php">Craft armore</a>
+    <br><a href="craft_tools/craft_tools_select.php">Craft tools</a>
 </body>
 </html>
