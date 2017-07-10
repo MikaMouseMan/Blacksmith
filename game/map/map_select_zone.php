a308177fa8257a449e56f6e07e024123ea8ef6a8
MikaMouseMan <n.mika.a@mail.ru>
2017-07-11 01:14:56 +0400

    add map "png" to images and set it to maps

--- game/map/map_select_zone.php
+++ game/map/map_select_zone.php
@@ -5,7 +5,10 @@
     <title>Blacksmith</title>
 </head>
 <body>
-   <a href="../exit.php">EXIT</a>
+    <a href="../exit.php">EXIT</a>
     <br><a href="../home/blacksmith_home.php">Blacksmith home</a>
+    <br><a href="world_map.php">World map</a>
+    <br><a href="work_zone.php">Work zone</a>
+    <br><a href="battle_map.php">Battle map</a>
 </body>
 </html>
---
+++ game/map/work_zone.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blacksmith</title>
+    <link rel="stylesheet" href='../../style/blacksmith.css'>
+</head>
+<body>
+    <a href="../exit.php">EXIT</a>
+    <a href="map_select_zone.php">Back</a>
+</body>
+</html>
