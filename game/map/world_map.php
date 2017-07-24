d334f641d4c8b8123e9844acb7a5e3f4cd1499e9
MikaMouseMan <n.mika.a@mail.ru>
2017-07-25 00:58:22 +0400

    Add drawing all map
    Too slow drawing need optimization

--- game/map/world_map.php
+++ game/map/world_map.php
@@ -1,12 +1,38 @@
+<?php
+
+set_time_limit(0);
+
+include("../../database/database.php");
+$select = mysql_query("SELECT * FROM `data_temp_map`");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Blacksmith</title>
-    <link rel="stylesheet" href="../../style/Blacksmith.css">
+    
 </head>
+
 <body>
-    <a style="color:red;" href="../exit.php">EXIT</a>
-    <br><a style="color:red;" href="map_select_zone.php">Back</a>
+    <a href="../exit.php">EXIT</a>
+    <br><a href="map_select_zone.php">Back</a><br>
+    <?
+    while($point = mysql_fetch_array($select)){
+
+        $r = $point['r'];
+        $g = $point['g'];
+        $b = $point['b'];
+        
+        echo "<a href = 'map_generator_100.php?x=".$point['x']."&y=".$point['y']."' style='background-color: RGB(".$r.",".$g.",".$b.")'>&#8195</a>";
+        
+        if($point['x']==511){
+            echo "<br>";
+        }
+
+    }
+    
+    
+    ?>
 </body>
 </html>
