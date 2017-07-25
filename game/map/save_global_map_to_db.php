f94ef332e04d980f20dc9f09cab4de8bd9927930
MikaMouseMan <n.mika.a@mail.ru>
2017-07-26 02:49:33 +0400

    renamed

--- game/map/save_global_map_to_db.php
+++ game/map/save_global_map_to_db.php
@@ -5,15 +5,15 @@ include ('../../database/database.php');
 
 $im = imagecreatefrompng("map_image.png");
 
-mysql_query("TRUNCATE data_temp_map");
+mysql_query("TRUNCATE data_map");
 
-$ji=0;
-for($i=0;$i<512;$i++){
+$ji = 0;
+for($i = 0; $i < 512; $i++){
     
     $values_string_new = "";
     $values_string_old = "";
     
-    for($j=0;$j<512;$j++){
+    for($j = 0; $j < 512; $j++){
         $rgb = imagecolorat($im, $j, $i);        
         $r = ($rgb >> 16) & 0xFF;
         $g = ($rgb >> 8) & 0xFF;
@@ -23,7 +23,7 @@ for($i=0;$i<512;$i++){
         $values_string_this = "('".$ji."', '".$j."', '".$i."', '".$r."', '".$g."', '".$b."')";
         if($values_string_old != ""){
             
-            $values_string_new = $values_string_old.",".$values_string_this;
+            $values_string_new = $values_string_old.", ".$values_string_this;
             
         }else{
             
@@ -33,7 +33,7 @@ for($i=0;$i<512;$i++){
         $values_string_old = $values_string_new;
     }
     
-    mysql_query("INSERT INTO `data_temp_map` (`id`, `x`, `y`, `r`, `g`, `b`) VALUES $values_string_new");
+    mysql_query("INSERT INTO `data_map` (`id`, `x`, `y`, `r`, `g`, `b`) VALUES $values_string_new");
 }
 
 ?>
