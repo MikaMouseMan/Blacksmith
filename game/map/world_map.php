1045343e36f0cff664145cc1c5c75d3aca6aa0ec
MikaMouseMan <n.mika.a@mail.ru>
2017-07-26 02:52:24 +0400

    Show all map, turned off

--- game/map/world_map.php
+++ game/map/world_map.php
@@ -3,7 +3,7 @@
 set_time_limit(0);
 
 include("../../database/database.php");
-$select = mysql_query("SELECT * FROM `data_temp_map`");
+$select = mysql_query("SELECT * FROM `data_map`");
 
 ?>
 <!DOCTYPE html>
@@ -14,9 +14,12 @@ $select = mysql_query("SELECT * FROM `data_temp_map`");
     
 </head>
 
-<body>
-    <a href="../exit.php">EXIT</a>
-    <br><a href="map_select_zone.php">Back</a><br>
+<body align = "center">    
+    <div>
+    <a style="color:red;" href="../exit.php">EXIT</a>
+    <br><a style="color:red;" href="../home/blacksmith_home.php">Blacksmith home</a>
+    <br><a style="color:red;" href="simple_player_global_state.php">World map</a>
+    </div>
     <?
     while($point = mysql_fetch_array($select)){
 
