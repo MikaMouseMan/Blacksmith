7d844104b753497d7d8eec987086ed2fd14bffc8
MikaMouseMan <n.mika.a@mail.ru>
2017-07-26 02:49:51 +0400

    renamed

--- game/map/simple_player_global_state.php
+++ game/map/simple_player_global_state.php
@@ -16,29 +16,32 @@ $form_user = "user_$user_name";
 $select = mysql_query("SELECT * FROM `$form_user` WHERE `cell_id` = '1001'");
 $player_coord = mysql_fetch_array($select);
 
-if($player_coord['health'] < map_drow_size){
+$player_x = (int)($player_coord['health'] / 1000000);
+$player_y = (int)($player_coord['health_max'] / 1000000);
+
+if($player_x < map_drow_size){
     $x_min = 0;
     $x_max = map_drow_size-1;
-}else if($player_coord['health'] >= map_x_max-map_drow_size-1){
+}else if($player_x >= map_x_max-map_drow_size-1){
     $x_max = map_x_max-1;
     $x_min = $x_max-map_drow_size;
 }else{
-    $x_min = (int)$player_coord['health']-map_drow_size/2;
-    $x_max = (int)$player_coord['health']+map_drow_size/2;
+    $x_min = (int)$player_x-map_drow_size/2;
+    $x_max = (int)$player_x+map_drow_size/2;
 }
 
-if($player_coord['health_max'] < map_drow_size){    
+if($player_y < map_drow_size){    
     $y_min = 0;
     $y_max = map_drow_size-1;
-}else if($player_coord['health_max'] >= map_y_max-map_drow_size-1){
+}else if($player_y >= map_y_max-map_drow_size-1){
     $y_max = map_y_max-1;
     $y_min = $y_max-map_drow_size;
 }else{
-    $y_min = (int)$player_coord['health_max']-map_drow_size/2;
-    $y_max = (int)$player_coord['health_max']+map_drow_size/2;
+    $y_min = (int)$player_y-map_drow_size/2;
+    $y_max = (int)$player_y+map_drow_size/2;
 }
 
-$select = mysql_query("SELECT * FROM `data_temp_map` WHERE `x` BETWEEN '$x_min' AND '$x_max' AND `y` BETWEEN '$y_min' AND '$y_max'");
+$select = mysql_query("SELECT * FROM `data_map` WHERE `x` BETWEEN '$x_min' AND '$x_max' AND `y` BETWEEN '$y_min' AND '$y_max'");
 
 ?>
 <!DOCTYPE html>
@@ -46,7 +49,7 @@ $select = mysql_query("SELECT * FROM `data_temp_map` WHERE `x` BETWEEN '$x_min' 
 <head>
     <meta charset="UTF-8">
     <title>Blacksmith</title>
-    
+    <link rel="stylesheet" href="../../style/blacksmith.css">
 </head>
 
 <style>
@@ -56,9 +59,13 @@ $select = mysql_query("SELECT * FROM `data_temp_map` WHERE `x` BETWEEN '$x_min' 
     }
 </style>
 
-<body>
-    <a href="../exit.php">EXIT</a>
-    <br><a href="map_select_zone.php">Back</a><br>
+<body align = "center">
+    <div>
+    <a style="color:red;" href="../exit.php">EXIT</a>
+    <br><a style="color:red;" href="../home/blacksmith_home.php">Blacksmith home</a>
+    <br><a style="color:red;" href="simple_player_global_state.php">World map</a>
+    </div>
+    <div style = "font-size: 10px">
     <?
     while($point = mysql_fetch_array($select)){
 
@@ -66,8 +73,8 @@ $select = mysql_query("SELECT * FROM `data_temp_map` WHERE `x` BETWEEN '$x_min' 
         $g = $point['g'];
         $b = $point['b'];
 
-        if($point['x']==$player_coord['health']&&$point['y']==$player_coord['health_max']){
-            echo "<a href = '' style='animation: state_1 2s infinite'>&#8195</a>";
+        if($point['x'] == $player_x && $point['y'] == $player_y){
+            echo "<a href = 'map_generator_100.php?x=".$point['x']."&y=".$point['y']."' style='animation: state_1 2s infinite'>&#8195</a>";
         }else{
             echo "<a href = 'map_generator_100.php?x=".$point['x']."&y=".$point['y']."' style='background-color: RGB(".$r.",".$g.",".$b.")'>&#8195</a>";
         }
@@ -76,8 +83,7 @@ $select = mysql_query("SELECT * FROM `data_temp_map` WHERE `x` BETWEEN '$x_min' 
         }
 
     }
-    
-    echo $x_max;
     ?>
+    </div>
 </body>
 </html>
