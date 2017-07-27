bc8e88a3f37cb856de6d4d70ed16187dd27d9413
MikaMouseMan <n.mika.a@mail.ru>
2017-07-27 21:41:36 +0400

    Building menu

---
+++ game/build/build_menu.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blacksmith</title>
+</head>
+<body>
+   <a href="../map/map_generator_10000.php">Back</a>
+    <a href="build.php?name=road">road</a>
+    <a href="build.php?name=floor">floor</a>
+    <a href="build.php?name=wall">wall</a>
+    <a href="build.php?name=door">door</a>
+    <a href="build.php?name=chest">chest</a>
+    <a href="">work structure</a>
+</body>
+</html>
