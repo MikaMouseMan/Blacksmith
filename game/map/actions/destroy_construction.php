96e4baaf77c6d3886a54c89d8271616ff1521b78
MikaMouseMan <n.mika.a@mail.ru>
2017-07-29 14:36:17 +0400

    destroy construction if it is player build it

---
+++ game/map/actions/destroy_construction.php
@@ -0,0 +1,18 @@
+<?php
+$x = $_POST['construction_x'];
+$y = $_POST['construction_y'];
+$master_id = $_POST['construction_master_id'];
+$player_id = $_POST['construction_player_id'];
+
+if($master_id != $player_id){
+    exit(header("Location: ../map_generator_10000.php?msg=Not you construction!"));
+}
+
+include('../../../database/database.php');
+
+$id = $x.$y;
+
+mysql_query("DELETE FROM `data_buildings_on_map` WHERE `data_buildings_on_map`.`id` = '$id'");
+
+header("Location: ../map_generator_10000.php?msg=Destroyed");
+?>
