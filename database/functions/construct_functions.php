7a47ecfbceaff57b3d4660b1f4ed298592bd225d
MikaMouseMan <n.mika.a@mail.ru>
2017-08-24 01:51:42 +0400

    add prototype construction functions

---
+++ database/functions/construct_functions.php
@@ -0,0 +1,48 @@
+<?php
+
+//////////////////////deconstruct
+function clear_cell($global_x, $global_y){
+    
+}
+
+///////////////point
+function build_road($global_x, $global_y, $player_id = 1){
+    
+}
+
+function build_wall($global_x, $global_y, $player_id = 1){
+    
+}
+
+function build_door($global_x, $global_y, $player_id = 1){
+    
+}
+
+function build_floor($global_x, $global_y, $player_id = 1){
+    
+}
+
+///////////////line/block
+function build_road_block($global_x1, $global_y1, $global_x2, $global_y2, $player_id = 1){
+    
+}
+
+function build_floor_block($global_x1, $global_y1, $global_x2, $global_y2, $player_id = 1){
+    
+}
+
+function build_wall_block($global_x1, $global_y1, $global_x2, $global_y2, $player_id = 1){
+    
+}
+
+//////////////empty home
+function build_home_empty($global_x1, $global_y1, $global_x2, $global_y2, $door_side, $player_id = 1){
+    
+}
+
+//////////////home
+function build_home($global_x1, $global_y1, $global_x2, $global_y2, $door_side, $player_id = 1){
+    
+}
+
+?>
