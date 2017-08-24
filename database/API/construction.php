628effe079861a0acda83e0522cfe1de64975b3c
MikaMouseMan <n.mika.a@mail.ru>
2017-08-25 02:44:20 +0400

    add to API link to construction function

---
+++ database/API/construction.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+if($_SESSION['user_id'] != 1){
+    exit(header("Location: ../../index.php"));
+}
+include ('../database.php');
+include('../functions/construct_functions.php');
+
+$user_id = $_SESSION['user_id'];
+$global_x = $_SESSION['x'];
+$global_y = $_SESSION['y'];
+
+
+build_home_empty($global_x - 4, $global_y - 4, $global_x + 4, $global_y + 4, "up", 1);
+
+?>
