70678c410bfd37c041605f4baf3a83e2657f47cd
MikaMouseMan <n.mika.a@mail.ru>
2017-08-28 01:41:13 +0400

    change ver number

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
    <br>
     <div>
         <a href="registration/login.php"><h1>LOG IN BLACKSMITH</h1></a>
-        <small>v.0.0.1</small>
+        <small>v.0.0.3</small>
     </div>
     <div>
         <small>26214400000000 block count</small>
